2b848f49723412883613cfd06282546e209c4be4
AnneJTL <anne.chatin@jointheleague.org>
2018-10-02 16:15:00 -0700

    CowTimer
    Thread.sleep(n)

--- src/CowTimer.java
+++ src/CowTimer.java
@@ -4,7 +4,6 @@
  */
 
 import java.applet.AudioClip;
-import java.io.IOException;
 import javax.swing.JApplet;
 
 public class CowTimer {
@@ -15,6 +14,9 @@ public class CowTimer {
 
 	public static void main(String[] args) throws InterruptedException {
 		/* 1. Make a CowTimer, set the time and start it. */
+		CowTimer aTimer = new CowTimer();
+		aTimer.setTime(6);
+		aTimer.start();
 
 	}
 
@@ -30,20 +32,29 @@ public class CowTimer {
 		 * 2. Count down the minutes, print the current minute then sleep for 60 seconds
 		 * using Thread.sleep(int milliseconds).
 		 */
+		for (int min = minutes; min > 0 ; min--){
+			System.out.println("minute: " + min);
+			// in minutes
+			// Thread.sleep(1000*60);
+
+			// in seconds
+			Thread.sleep(1000);
+		}
 
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
 		 * You can use the .wav file in the default package, or you can download one
-		 * from freesound.org, then drag it intothe default package.
+		 * from freesound.org, then drag it into the default package.
 		 */
-
+		playSound("creepy-noise.wav");
 	}
 
 	private void playSound(String fileName) {
 		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		System.out.println(sound.toString());
 		sound.play();
 	}
-
+/*
 	private void speak(String stuffToSay) {
 		try {
 			Runtime.getRuntime().exec("say " + stuffToSay).waitFor();
@@ -51,5 +62,5 @@ public class CowTimer {
 			e.printStackTrace();
 		}
 	}
-
+*/
 }
